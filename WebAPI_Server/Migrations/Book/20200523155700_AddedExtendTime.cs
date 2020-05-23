eee808eab99e4f731e813d856d189dc76d381790
TheUsernameIsNotTaken <pixelbetyar77@gmail.com>
2020-05-23 17:57:45 +0200

    Updated Database

---
+++ WebAPI_Server/Migrations/Book/20200523155700_AddedExtendTime.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI_Server.Migrations.Book
+{
+    public partial class AddedExtendTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "TimesExtended",
+                table: "Books",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimesExtended",
+                table: "Books");
+        }
+    }
+}
